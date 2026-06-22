b29d10ba0c7d679493a135c8545759aad43d3276
joe <allenstanleewest@gmail.com>
2026-06-22 11:55:37 +0300

    fix: increase logo to h-12

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -24,7 +24,7 @@ export function Navbar() {
             className="hover:opacity-70 transition-opacity"
             onClick={() => setOpen(false)}
           >
-            <img src="/logo.png" alt="kikr" className="h-10 w-auto" />
+            <img src="/logo.png" alt="kikr" className="h-12 w-auto" />
           </Link>
 
           <div className="hidden md:flex items-center gap-1">
